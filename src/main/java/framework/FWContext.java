e72278b41ba8ea13c1d0d714bedf763a2c82fcfb
phatvoong <thephat296@gmail.com>
2023-07-16 17:00:32 -0500

    refactor code

--- src/main/java/framework/FWContext.java
+++ src/main/java/framework/FWContext.java
@@ -27,65 +27,76 @@ public class FWContext {
     }
 
     private void performDI() {
-        try {
-            for (Class<?> clazz : beans.keySet()) {
-                Object bean = beans.get(clazz);
-                /* constructor injection */
-                Constructor<?>[] constructors = clazz.getConstructors();
-                if (constructors.length == 1 && constructors[0].isAnnotationPresent(Autowired.class)) {
-                    Constructor<?> constructor = constructors[0];
-                    Class<?>[] parameterTypes = constructor.getParameterTypes();
-                    Object[] dependencies = new Object[parameterTypes.length];
-                    for (int i = 0; i < parameterTypes.length; i++) {
-                        dependencies[i] = resolveDependency(parameterTypes[i], constructor.getParameters()[i].getAnnotation(Qualifier.class));
-                        if (dependencies[i] == null) {
-                            throw new RuntimeException("Dependency not found for constructor parameter: " + constructor.getName());
-                        }
-                    }
-                    try {
-                        bean = constructor.newInstance(dependencies);
-                        beans.put(clazz, bean);
-                    } catch (Exception e) {
-                        throw new RuntimeException("Failed to perform dependency injection for constructor: " + constructor.getName(), e);
-                    }
-                }
-
-                /* field injection */
-                for (Field field : clazz.getDeclaredFields()) {
-                    if (field.isAnnotationPresent(Autowired.class)) {
-                        field.setAccessible(true);
-                        field.set(bean, beans.get(field.getType()));
-                    }
-                }
-
-                /* setter injection */
-                for (Method method : clazz.getMethods()) {
-                    if (method.isAnnotationPresent(Autowired.class) && method.getName().startsWith("set")) {
-                        Class<?>[] parameterTypes = method.getParameterTypes();
-                        if (parameterTypes.length != 1) {
-                            throw new RuntimeException("Setter method should have exactly one parameter: " + method.getName());
-                        }
-                        Object dependency = resolveDependency(parameterTypes[0], method.getAnnotation(Qualifier.class));
-                        if (dependency == null) {
-                            throw new RuntimeException("Dependency not found for setter method: " + method.getName());
-                        }
-                        try {
-                            method.invoke(bean, dependency);
-                        } catch (Exception e) {
-                            throw new RuntimeException("Failed to perform dependency injection for setter method: " + method.getName(), e);
-                        }
-                    }
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        for (Class<?> clazz : beans.keySet()) {
+            Object bean = beans.get(clazz);
+            injectConstructor(clazz, bean);
+            injectField(clazz, bean);
+            injectSetter(clazz, bean);
         }
     }
 
-    private Object resolveDependency(Class<?> dependencyType, Qualifier qualifierAnnotation) throws ClassNotFoundException {
+    private void injectConstructor(Class<?> clazz, Object bean) {
+        Constructor<?>[] constructors = clazz.getConstructors();
+        if (constructors.length == 1 && constructors[0].isAnnotationPresent(Autowired.class)) {
+            Constructor<?> constructor = constructors[0];
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            Object[] dependencies = new Object[parameterTypes.length];
+            for (int i = 0; i < parameterTypes.length; i++) {
+                dependencies[i] = resolveDependency(parameterTypes[i], constructor.getParameters()[i].getAnnotation(Qualifier.class));
+                if (dependencies[i] == null) {
+                    throw new RuntimeException("Dependency not found for constructor parameter: " + constructor.getName());
+                }
+            }
+            try {
+                bean = constructor.newInstance(dependencies);
+                beans.put(clazz, bean);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to perform dependency injection for constructor: " + constructor.getName(), e);
+            }
+        }
+    }
+
+    private void injectField(Class<?> clazz, Object bean) {
+        for (Field field : clazz.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Autowired.class)) {
+                field.setAccessible(true);
+                try {
+                    field.set(bean, beans.get(field.getType()));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void injectSetter(Class<?> clazz, Object bean) {
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(Autowired.class) && method.getName().startsWith("set")) {
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                if (parameterTypes.length != 1) {
+                    throw new RuntimeException("Setter method should have exactly one parameter: " + method.getName());
+                }
+                Object dependency = resolveDependency(parameterTypes[0], method.getAnnotation(Qualifier.class));
+                if (dependency == null) {
+                    throw new RuntimeException("Dependency not found for setter method: " + method.getName());
+                }
+                try {
+                    method.invoke(bean, dependency);
+                } catch (Exception e) {
+                    throw new RuntimeException("Failed to perform dependency injection for setter method: " + method.getName(), e);
+                }
+            }
+        }
+    }
+
+    private Object resolveDependency(Class<?> dependencyType, Qualifier qualifierAnnotation) {
         if (qualifierAnnotation != null) {
             String className = qualifierAnnotation.value();
-            return beans.get(Class.forName(className));
+            try {
+                return beans.get(Class.forName(className));
+            } catch (ClassNotFoundException e) {
+                throw null;
+            }
         } else {
             return beans.values().stream()
                     .filter(bean -> bean != null && dependencyType.isAssignableFrom(bean.getClass()))
