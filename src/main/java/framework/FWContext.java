da8a9a7d8492c6c81d04db70ae2f64be8f18bfd1
phatvoong <thephat296@gmail.com>
2023-07-16 16:34:42 -0500

    add qualifier

--- src/main/java/framework/FWContext.java
+++ src/main/java/framework/FWContext.java
@@ -2,7 +2,9 @@ package framework;
 
 import org.reflections.Reflections;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -27,15 +29,68 @@ public class FWContext {
     private void performDI() {
         try {
             for (Object bean : beans.values()) {
-                for (Field field : bean.getClass().getDeclaredFields()) {
+                Class<?> clazz = bean.getClass();
+                /* field injection */
+                for (Field field : clazz.getDeclaredFields()) {
                     if (field.isAnnotationPresent(Autowired.class)) {
                         field.setAccessible(true);
                         field.set(bean, beans.get(field.getType()));
                     }
                 }
+
+                /* setter injection */
+                for (Method method : clazz.getMethods()) {
+                    if (method.isAnnotationPresent(Autowired.class) && method.getName().startsWith("set")) {
+                        Class<?>[] parameterTypes = method.getParameterTypes();
+                        if (parameterTypes.length != 1) {
+                            throw new RuntimeException("Setter method should have exactly one parameter: " + method.getName());
+                        }
+                        Object dependency = resolveDependency(parameterTypes[0], method.getAnnotation(Qualifier.class));
+                        if (dependency == null) {
+                            throw new RuntimeException("Dependency not found for setter method: " + method.getName());
+                        }
+                        try {
+                            method.invoke(bean, dependency);
+                        } catch (Exception e) {
+                            throw new RuntimeException("Failed to perform dependency injection for setter method: " + method.getName(), e);
+                        }
+                    }
+                }
+
+                /* constructor injection */
+                Constructor<?>[] constructors = clazz.getConstructors();
+                if (constructors.length == 1 && constructors[0].isAnnotationPresent(Autowired.class)) {
+                    Constructor<?> constructor = constructors[0];
+                    Class<?>[] parameterTypes = constructor.getParameterTypes();
+                    Object[] dependencies = new Object[parameterTypes.length];
+                    for (int i = 0; i < parameterTypes.length; i++) {
+                        dependencies[i] = resolveDependency(parameterTypes[i], constructor.getParameters()[i].getAnnotation(Qualifier.class));
+                        if (dependencies[i] == null) {
+                            throw new RuntimeException("Dependency not found for constructor parameter: " + constructor.getName());
+                        }
+                    }
+                    try {
+                        bean = constructor.newInstance(dependencies);
+                        beans.put(clazz, bean);
+                    } catch (Exception e) {
+                        throw new RuntimeException("Failed to perform dependency injection for constructor: " + constructor.getName(), e);
+                    }
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    private Object resolveDependency(Class<?> dependencyType, Qualifier qualifierAnnotation) throws ClassNotFoundException {
+        if (qualifierAnnotation != null) {
+            String className = qualifierAnnotation.value();
+            return beans.get(Class.forName(className));
+        } else {
+            return beans.values().stream()
+                    .filter(bean -> dependencyType.isAssignableFrom(bean.getClass()))
+                    .findFirst()
+                    .orElse(null);
         }
     }
 
